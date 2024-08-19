7d54ac1f5f84eb3dde641eed0b6bb7f78e80375f
LENGA_TAKAMURA <kotodaman55324678@gmail.com>
2024-08-19 17:32:33 +0900

    [Add]進行方向に向きを変えるメソッドの作成　改善の余地あり

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -12,6 +12,8 @@ public class PlayerMove : MonoBehaviour
     Animator m_anim;
     bool _isGround;
 
+    
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -20,10 +22,13 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        LookAhead();
+        //Vector3 defalut = transform.position - lastPos;
+        //lastPos = transform.position;
         //地面についている時
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
             //Transform.TransformDirectionはローカル空間からワールド空間へ方向Vectorを変換する
             moveDirection = transform.TransformDirection(moveDirection) * speed;
             //ジャンプ↓
@@ -35,6 +40,8 @@ public class PlayerMove : MonoBehaviour
         //重力分変更する
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+        
+       
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -61,5 +68,18 @@ public class PlayerMove : MonoBehaviour
 
         }
         
+    }
+
+    void LookAhead()
+    {
+
+        if(moveDirection.magnitude == 0)
+        {
+            return;
+        }
+        else if (moveDirection.magnitude > 0.1f) 
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
     }
 }
